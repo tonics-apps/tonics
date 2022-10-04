5c65ed01b35b4bffd01dc8b0434a3fe6ff1f3b2c
DevsrealmGuy <faruq@devsrealm.com>
2022-10-04 13:41:16 +0100

    Update Changes

--- src/Apps/NinetySeven/NinetySevenActivator.php
+++ src/Apps/NinetySeven/NinetySevenActivator.php
@@ -143,49 +143,56 @@ class NinetySevenActivator implements ExtensionConfig
     "field_name": "modular_rowcolumn",
     "field_id": 1,
     "field_parent_id": null,
-    "field_options": "{\"field_slug\":\"modular_rowcolumn\",\"field_slug_unique_hash\":\"35iwumwke7e000000000\",\"fieldName\":\"site_logo_site_nav\",\"inputName\":\"\",\"row\":\"1\",\"column\":\"2\",\"hideInUserEditForm\":\"0\",\"cell\":\"on\"}"
+    "field_options": "{\"field_validations\":[],\"field_slug\":\"modular_rowcolumn\",\"field_slug_unique_hash\":\"202kaq8fyz5s000000000\",\"fieldName\":\"Settings\",\"inputName\":\"\",\"row\":\"2\",\"column\":\"2\",\"grid_template_col\":\"\",\"hideInUserEditForm\":\"0\",\"useTab\":\"1\",\"group\":\"0\",\"cell\":\"on\"}"
+  },
+  {
+    "fk_field_id": "App Ninety Seven Settings",
+    "field_name": "modular_rowcolumn",
+    "field_id": 2,
+    "field_parent_id": 1,
+    "field_options": "{\"field_slug\":\"modular_rowcolumn\",\"modular_rowcolumn_cell\":\"1\",\"field_slug_unique_hash\":\"35iwumwke7e000000000\",\"fieldName\":\"Logo and Navigation\",\"inputName\":\"\",\"row\":\"1\",\"column\":\"2\",\"grid_template_col\":\"2fr 3fr\",\"hideInUserEditForm\":\"0\",\"useTab\":\"0\",\"group\":\"0\",\"cell\":\"on\"}"
   },
   {
     "fk_field_id": "App Ninety Seven Settings",
     "field_name": "media_media-image",
-    "field_id": 2,
-    "field_parent_id": 1,
+    "field_id": 3,
+    "field_parent_id": 2,
     "field_options": "{\"field_slug\":\"media_media-image\",\"media_media-image_cell\":\"1\",\"field_slug_unique_hash\":\"4v3issqqxls0000000000\",\"fieldName\":\"site_logo\",\"inputName\":\"site_logo\",\"imageLink\":\"/logo/o-ola-micky-logo.svg\",\"featured_image\":\"\",\"defaultImage\":\"\"}"
   },
   {
     "fk_field_id": "App Ninety Seven Settings",
     "field_name": "menu_menus",
-    "field_id": 3,
-    "field_parent_id": 1,
+    "field_id": 4,
+    "field_parent_id": 2,
     "field_options": "{\"field_slug\":\"menu_menus\",\"menu_menus_cell\":\"2\",\"field_slug_unique_hash\":\"7ozvcwh4f4w000000000\",\"fieldName\":\"site_header_menu\",\"inputName\":\"site_header_menu\",\"menuSlug\":\"header-menu:1\",\"displayName\":\"1\"}"
   },
   {
     "fk_field_id": "App Ninety Seven Settings",
     "field_name": "widget_widgets",
-    "field_id": 4,
-    "field_parent_id": null,
-    "field_options": "{\"field_slug\":\"widget_widgets\",\"field_slug_unique_hash\":\"6kcp1ee1xu00000000000\",\"fieldName\":\"sidebar_widget\",\"inputName\":\"sidebar_widget\",\"widgetSlug\":\"sidebar-widget:1\"}"
+    "field_id": 5,
+    "field_parent_id": 1,
+    "field_options": "{\"field_slug\":\"widget_widgets\",\"widget_widgets_cell\":\"2\",\"field_slug_unique_hash\":\"6kcp1ee1xu00000000000\",\"fieldName\":\"Sidebar Widget\",\"inputName\":\"sidebar_widget\",\"widgetSlug\":\"sidebar-widget:1\"}"
   },
   {
     "fk_field_id": "App Ninety Seven Settings",
     "field_name": "modular_rowcolumn",
-    "field_id": 5,
-    "field_parent_id": null,
-    "field_options": "{\"field_validations\":[],\"field_slug\":\"modular_rowcolumn\",\"field_slug_unique_hash\":\"5xhyxqw7s1g0000000000\",\"fieldName\":\"site_footer\",\"inputName\":\"\",\"row\":\"2\",\"column\":\"1\",\"hideInUserEditForm\":\"0\",\"cell\":\"on\"}"
+    "field_id": 6,
+    "field_parent_id": 1,
+    "field_options": "{\"field_validations\":[],\"field_slug\":\"modular_rowcolumn\",\"modular_rowcolumn_cell\":\"3\",\"field_slug_unique_hash\":\"5xhyxqw7s1g0000000000\",\"fieldName\":\"Site Footer\",\"inputName\":\"\",\"row\":\"2\",\"column\":\"1\",\"grid_template_col\":\"1\",\"hideInUserEditForm\":\"0\",\"useTab\":\"0\",\"group\":\"0\",\"cell\":\"on\"}"
   },
   {
     "fk_field_id": "App Ninety Seven Settings",
     "field_name": "menu_menus",
-    "field_id": 6,
-    "field_parent_id": 5,
+    "field_id": 7,
+    "field_parent_id": 6,
     "field_options": "{\"field_slug\":\"menu_menus\",\"menu_menus_cell\":\"1\",\"field_slug_unique_hash\":\"6mkrfyi63z00000000000\",\"fieldName\":\"site_footer\",\"inputName\":\"site_footer_menu\",\"menuSlug\":\"footer-menu:2\",\"displayName\":\"0\"}"
   },
   {
     "fk_field_id": "App Ninety Seven Settings",
     "field_name": "input_rich-text",
-    "field_id": 7,
-    "field_parent_id": 5,
-    "field_options": "{\"field_validations\":[],\"field_slug\":\"input_rich-text\",\"input_rich-text_cell\":\"2\",\"field_slug_unique_hash\":\"6h1171hdkzk0000000000\",\"fieldName\":\"site_credit\",\"inputName\":\"site_credit\",\"maxChar\":\"\",\"placeholder\":\"\",\"readOnly\":\"0\",\"required\":\"1\",\"defaultValue\":\"<span class=\\\"site-footer-info\\\"> © 2022 Devsrealm |        <a href=\\\"https://devsrealm.com/\\\">Powered by Tonics</a> | Theme: <a href=\\\"#\\\">NinetySeven by DevsRealmGuy</a>    </span>\"}"
+    "field_id": 8,
+    "field_parent_id": 6,
+    "field_options": "{\"field_validations\":[],\"field_slug\":\"input_rich-text\",\"input_rich-text_cell\":\"2\",\"field_slug_unique_hash\":\"6h1171hdkzk0000000000\",\"fieldName\":\"site_credit\",\"inputName\":\"site_credit\",\"defaultValue\":\"<span class=\\\"site-footer-info\\\"> © 2022 Devsrealm |        <a href=\\\"https://devsrealm.com/\\\">Powered by Tonics</a> | Theme: <a href=\\\"#\\\">NinetySeven by DevsRealmGuy</a>    </span>\",\"hideInUserEditForm\":\"0\",\"maxChar\":\"\",\"placeholder\":\"\",\"readOnly\":\"0\",\"required\":\"1\"}"
   }
 ]
 JSON;
