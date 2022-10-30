98bad15fe241c1db0d5e4b130a738162d2a37bec
DevsrealmGuy <faruq@devsrealm.com>
2022-10-30 12:59:52 +0100

    Update Changes

--- src/Apps/TonicsAmazonAffiliate/Controller/TonicsAmazonAffiliateController.php
+++ src/Apps/TonicsAmazonAffiliate/Controller/TonicsAmazonAffiliateController.php
@@ -17,6 +17,10 @@ use Amazon\ProductAdvertisingAPI\v1\com\amazon\paapi5\v1\GetItemsResource;
 use Amazon\ProductAdvertisingAPI\v1\com\amazon\paapi5\v1\PartnerType;
 use Amazon\ProductAdvertisingAPI\v1\com\amazon\paapi5\v1\ProductAdvertisingAPIClientException;
 use Amazon\ProductAdvertisingAPI\v1\Configuration;
+use App\Modules\Core\Configs\AppConfig;
+use App\Modules\Core\Configs\FieldConfig;
+use App\Modules\Core\Library\Authentication\Session;
+use App\Modules\Field\Data\FieldData;
 
 require dirname(__FILE__, 2) . '/Library/AmazonPAA/paapi5/vendor/autoload.php';
 
@@ -25,24 +29,20 @@ class TonicsAmazonAffiliateController
     private string $accessKey = '';
     private string $secretKey = '';
     private string $partnerTag = '';
-    
-    private ?Configuration $configuration = null;
 
-    public function __construct()
+    const CACHE_KEY = 'TonicsPlugin_TonicsAmazonAffiliateSettings';
+
+    const SETTINGS_ACCESS_KEY_INPUT_NAME = 'tonicsAmazonAffiliateSettings_apiKey';
+    const SETTINGS_SECRET_KEY_INPUT_NAME = 'tonicsAmazonAffiliateSettings_apiSecret';
+    const SETTINGS_PARTNER_TAG_INPUT_NAME = 'tonicsAmazonAffiliateSettings_partnerTag';
+    const SETTINGS_REGION_INPUT_NAME = 'tonicsAmazonAffiliateSettings_region';
+
+    private ?Configuration $configuration;
+    private ?FieldData $fieldData;
+
+    public function __construct(FieldData $fieldData = null)
     {
-        $this->accessKey = 'AKIAJB5WXDLIJNIYHLWQ';
-        $this->secretKey = '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';
-        $this->partnerTag = 'exclu25401-20';
-
-        $this->configuration = new Configuration();
-
-        /*
-         * Add your credentials
-         */
-        # Please add your access key here
-        $this->configuration->setAccessKey($this->accessKey);
-        # Please add your secret key here
-        $this->configuration->setSecretKey($this->secretKey);
+        $this->fieldData = $fieldData;
 
         # Please add your partner tag (store/tracking id) here
         $partnerTag = $this->partnerTag;
@@ -54,130 +54,65 @@ class TonicsAmazonAffiliateController
          */
         $this->configuration->setHost('webservices.amazon.com');
         $this->configuration->setRegion('us-east-1');
+    }
 
-        $apiInstance = new DefaultApi(
-        /*
-         * If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
-         * This is optional, `GuzzleHttp\Client` will be used as default.
-         */
-            new \GuzzleHttp\Client(),
-            $this->configuration
-        );
+    public function getAmazonConfiguration()
+    {
 
-        # Request initialization
+        $settings = self::getSettingsData();
+        $this->accessKey = $settings[self::SETTINGS_ACCESS_KEY_INPUT_NAME] ?? '';
+        $this->secretKey = $settings[self::SETTINGS_SECRET_KEY_INPUT_NAME] ?? '';
+        $this->partnerTag = $settings[self::SETTINGS_PARTNER_TAG_INPUT_NAME] ?? '';
+        $region = trim($settings[self::SETTINGS_REGION_INPUT_NAME] ?? '');
 
-        # Choose item id(s)
-        $itemIds = ["059035342X", "B00X4WHP55", "1401263119", "0136816487", 'B01EJ5UV3C'];
-
-        /*
-         * Choose resources you want from GetItemsResource enum
-         * For more details, refer: https://webservices.amazon.com/paapi5/documentation/get-items.html#resources-parameter
-         */
-        $resources = [
-            GetItemsResource::ITEM_INFOTITLE,
-            GetItemsResource::ITEM_INFOBY_LINE_INFO,
-            GetItemsResource::ITEM_INFOFEATURES,
-            GetItemsResource::ITEM_INFOPRODUCT_INFO,
-            GetItemsResource::ITEM_INFOTECHNICAL_INFO,
-            GetItemsResource::OFFERSLISTINGSDELIVERY_INFOIS_AMAZON_FULFILLED,
-            GetItemsResource::OFFERSLISTINGSDELIVERY_INFOIS_PRIME_ELIGIBLE,
-            GetItemsResource::OFFERSLISTINGSPRICE,
-            GetItemsResource::OFFERSLISTINGSPROMOTIONS,
-            GetItemsResource::OFFERSLISTINGSSAVING_BASIS,
-            GetItemsResource::CUSTOMER_REVIEWSCOUNT,
-            GetItemsResource::CUSTOMER_REVIEWSSTAR_RATING,
-            GetItemsResource::IMAGESPRIMARYLARGE,
-            GetItemsResource::IMAGESPRIMARYMEDIUM,
-            GetItemsResource::IMAGESPRIMARYSMALL,
-            GetItemsResource::IMAGESVARIANTSLARGE,
-            GetItemsResource::IMAGESVARIANTSMEDIUM,
-            GetItemsResource::IMAGESVARIANTSSMALL,
+        $regionData = [
+            'USA' => [
+                'HOST' => '',
+                'REGION' => '',
+            ]
         ];
 
-        # Forming the request
-        $getItemsRequest = new GetItemsRequest();
-        $getItemsRequest->setItemIds($itemIds);
-        $getItemsRequest->setPartnerTag($partnerTag);
-        $getItemsRequest->setPartnerType(PartnerType::ASSOCIATES);
-        $getItemsRequest->setResources($resources);
+        $this->configuration = new Configuration();
 
-        # Validating request
-        $invalidPropertyList = @$getItemsRequest->listInvalidProperties();
-        $length = count($invalidPropertyList);
-        if ($length > 0) {
-            echo "Error forming the request", PHP_EOL;
-            foreach ($invalidPropertyList as $invalidProperty) {
-                echo $invalidProperty, PHP_EOL;
-            }
-            return;
-        }
+        /*
+         * Add your credentials
+         */
+        # Please add your access key here
+        $this->configuration->setAccessKey($this->accessKey);
+        # Please add your secret key here
+        $this->configuration->setSecretKey($this->secretKey);
+    }
 
-        # Sending the request
+    /**
+     * @throws \Exception
+     */
+    public function edit(): void
+    {
+        $fieldItems = $this->getFieldData()->generateFieldWithFieldSlug(
+            ['app-tonicsamazonaffiliate-settings'],
+            $this->getSettingsData()
+        )->getHTMLFrag();
+
+        view('Apps::TonicsAmazonAffiliate/Views/settings', [
+                'FieldItems' => $fieldItems,
+            ]
+        );
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function update()
+    {
         try {
-            $getItemsResponse = @$apiInstance->getItems($getItemsRequest);
-           // dd($getItemsResponse);
-
-            // echo 'API called successfully', PHP_EOL;
-           // echo 'Complete Response: ', $getItemsResponse, PHP_EOL;
-
-            # Parsing the response
-            if ($getItemsResponse->getItemsResult() !== null) {
-                if ($getItemsResponse->getItemsResult()->getItems() !== null) {
-                    $responseList = $this->parseResponse($getItemsResponse->getItemsResult()->getItems());
-                     dd($responseList, $itemIds);
-
-                    foreach ($itemIds as $itemId) {
-                        echo 'Printing information about the itemId: ', $itemId, PHP_EOL;
-                        $item = $responseList[$itemId];
-                        if ($item !== null) {
-                            if ($item->getASIN()) {
-                                echo 'ASIN: ', $item->getASIN(), PHP_EOL;
-                            }
-                            if ($item->getItemInfo() !== null and $item->getItemInfo()->getTitle() !== null
-                                and $item->getItemInfo()->getTitle()->getDisplayValue() !== null) {
-                                echo 'Title: ', $item->getItemInfo()->getTitle()->getDisplayValue(), PHP_EOL;
-                            }
-                            if ($item->getDetailPageURL() !== null) {
-                                echo 'Detail Page URL: ', $item->getDetailPageURL(), PHP_EOL;
-                            }
-                            if ($item->getOffers() !== null and
-                                $item->getOffers()->getListings() !== null
-                                and $item->getOffers()->getListings()[0]->getPrice() !== null
-                                and $item->getOffers()->getListings()[0]->getPrice()->getDisplayAmount() !== null) {
-                                echo 'Buying price: ', $item->getOffers()->getListings()[0]->getPrice()
-                                    ->getDisplayAmount(), PHP_EOL;
-                            }
-                        } else {
-                            echo "Item not found, check errors", PHP_EOL;
-                        }
-                    }
-                }
-            }
-            if ($getItemsResponse->getErrors() !== null) {
-                // Error Code and Error Message
-                // $getItemsResponse->getErrors()[0]->getCode()
-                // $getItemsResponse->getErrors()[0]->getMessage()
-            }
-        } catch (ApiException $exception) {
-            // Error Code and Error Message
-            // $exception->getCode()
-            // $exception->getMessage()
-            if ($exception->getResponseObject() instanceof ProductAdvertisingAPIClientException) {
-                $errors = $exception->getResponseObject()->getErrors();
-                // Multiple Errors
-                foreach ($errors as $error) {
-                    // $error->getCode()
-                    // $error->getMessage()
-                }
-            } else {
-                // Error Response Body
-                // $exception->getResponseBody()
-            }
-        } catch (\Exception $exception) {
-            // Error Regular PHP Exception
-            // $exception->getMessage()
+            $settings = FieldConfig::savePluginFieldSettings(self::getCacheKey(), $_POST);
+            apcu_store(self::getCacheKey(), $settings);
+            session()->flash(['Settings Updated'], type: Session::SessionCategories_FlashMessageSuccess);
+            redirect(route('tonicsAmazonAffiliate.settings'));
+        }catch (\Exception){
+            session()->flash(['An Error Occurred Saving Settings'], $_POST);
+            redirect(route('tonicsAmazonAffiliate.settings'));
         }
-        
     }
 
     /**
@@ -193,5 +128,31 @@ class TonicsAmazonAffiliateController
             $mappedResponse[$item->getASIN()] = $item;
         }
         return $mappedResponse;
+    }
+
+    /**
+     * @return FieldData|null
+     */
+    public function getFieldData(): ?FieldData
+    {
+        return $this->fieldData;
+    }
+
+    public static function getCacheKey(): string
+    {
+        return AppConfig::getAppCacheKey() . self::CACHE_KEY;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public static function getSettingsData()
+    {
+        $settings = apcu_fetch(self::getCacheKey());
+        if ($settings === false){
+            $settings = FieldConfig::loadPluginSettings(self::getCacheKey());
+        }
+
+        return $settings;
     }
 }
