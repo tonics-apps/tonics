d170c5bad578351853dd9f996984b181cd0baba8
DevsrealmGuy <faruq@devsrealm.com>
2022-12-10 09:48:07 +0100

    Update Changes

--- src/Modules/Core/Controllers/JobManagerController.php
+++ src/Modules/Core/Controllers/JobManagerController.php
@@ -30,24 +30,20 @@ class JobManagerController
         $dataTableHeaders = [
             ['type' => '', 'slug' => Tables::SCHEDULER . '::' . 'schedule_id', 'title' => 'ID', 'minmax' => '50px, .5fr', 'td' => 'schedule_id'],
             ['type' => '', 'slug' => Tables::SCHEDULER . '::' . 'schedule_name', 'title' => 'Title', 'minmax' => '150px, 1.6fr', 'td' => 'schedule_name'],
-            ['type' => '', 'slug' => Tables::SCHEDULER . '::' . 'schedule_parent_name', 'title' => 'Depends On', 'minmax' => '150px, 1.6fr', 'td' => 'schedule_parent_name'],
+            ['type' => '', 'slug' => Tables::SCHEDULER . '::' . 'schedule_parent_name', 'title' => 'Depends On', 'minmax' => '100px, 1fr', 'td' => 'schedule_parent_name'],
             ['type' => '', 'slug' => Tables::SCHEDULER . '::' . 'schedule_ticks', 'title' => 'Ticks', 'minmax' => '50px, .5fr', 'td' => 'schedule_ticks'],
             ['type' => '', 'slug' => Tables::SCHEDULER . '::' . 'schedule_priority', 'title' => 'Priority', 'minmax' => '50px, .5fr', 'td' => 'schedule_priority'],
-            ['type' => '', 'slug' => Tables::SCHEDULER . '::' . 'schedule_every', 'title' => 'Schedule Every', 'minmax' => '50px, .5fr', 'td' => 'schedule_every'],
-            ['type' => 'date_time_local', 'slug' => Tables::SCHEDULER . '::' . 'schedule_next_run', 'title' => 'Next Run', 'minmax' => '150px, 1fr', 'td' => 'schedule_next_run'],
+            ['type' => '', 'slug' => Tables::SCHEDULER . '::' . 'schedule_every', 'title' => 'Schedule Every', 'minmax' => '10px, 1fr', 'td' => 'schedule_every'],
+            ['type' => '', 'slug' => Tables::SCHEDULER . '::' . 'schedule_next_run', 'title' => 'Next Run', 'minmax' => '150px, 1fr', 'td' => 'schedule_next_run'],
         ];
 
         $data = db()->Select('*')
             ->From($table)
             ->when(url()->hasParamAndValue('query'), function (TonicsQuery $db) {
                 $db->WhereLike('schedule_name', url()->getParam('query'));
+            })->OrderByAsc(table()->pickTable($table, ['schedule_next_run']))->SimplePaginate(url()->getParam('per_page', AppConfig::getAppPaginationMax()));
 
-            })->when(url()->hasParamAndValue('start_date') && url()->hasParamAndValue('end_date'), function (TonicsQuery $db) use ($table) {
-                $db->WhereBetween(table()->pickTable($table, ['created_at']), db()->DateFormat(url()->getParam('start_date')), db()->DateFormat(url()->getParam('end_date')));
-
-            })->OrderByDesc(table()->pickTable($table, ['updated_at']))->SimplePaginate(url()->getParam('per_page', AppConfig::getAppPaginationMax()));
-
-        view('Modules::Track/Views/License/index', [
+        view('Modules::Core/Views/JobsManager/jobs_scheduler_index', [
             'DataTable' => [
                 'headers' => $dataTableHeaders,
                 'paginateData' => $data ?? []
