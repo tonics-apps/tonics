8eb1358a01ba92a02016f47b7d68345a5b613322
DevsrealmGuy <faruq@devsrealm.com>
2022-12-25 08:16:25 +0100

    Update Changes

---
+++ src/Modules/Track/Database/Migrations/CreateTracksCategories_2022_12_25_080501.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Copyright (c) 2022. Ahmed Olayemi Faruq <faruq@devsrealm.com>
+ */
+
+namespace App\Modules\Track\Database\Migrations;
+
+use App\Modules\Core\Library\Migration;
+use App\Modules\Core\Library\Tables;
+
+class CreateTracksCategories_2022_12_25_080501 extends Migration {
+
+    public function up()
+    {
+        $this->getDB()->run("");
+    }
+
+    public function down()
+    {
+        $this->getDB()->run("");
+    }
+}
