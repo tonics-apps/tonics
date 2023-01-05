798f33c96c44d631294160a506e1e81666a5b089
DevsrealmGuy <faruq@devsrealm.com>
2023-01-05 21:34:49 +0100

    Update Changes

--- src/Apps/NinetySeven/EventHandler/PageTemplates/BeatsTonics/ThemeFolder/TonicsNinetySevenBeatsTonicsThemeFolderTrackCategoryTemplate.php
+++ src/Apps/NinetySeven/EventHandler/PageTemplates/BeatsTonics/ThemeFolder/TonicsNinetySevenBeatsTonicsThemeFolderTrackCategoryTemplate.php
@@ -17,6 +17,7 @@ use App\Modules\Page\Events\AbstractClasses\PageTemplateInterface;
 use App\Modules\Page\Events\OnPageTemplate;
 use App\Modules\Track\Data\TrackData;
 use Devsrealm\TonicsEventSystem\Interfaces\HandlerInterface;
+use Devsrealm\TonicsQueryBuilder\TonicsQuery;
 use RecursiveArrayIterator;
 use RecursiveIteratorIterator;
 
@@ -61,48 +62,102 @@ class TonicsNinetySevenBeatsTonicsThemeFolderTrackCategoryTemplate implements Pa
             $licenseTable = Tables::getTable(Tables::LICENSES);
             $artistTable = Tables::getTable(Tables::ARTISTS);
 
-            $data = db()->Select('*')->From(db()->Select("t.track_id as id, t.slug_id, t.track_title as _name, null as num_tracks, t.track_plays as plays,
+            try {
+                $db = db();
+
+                $db->Select('*')->From(db()->Select("t.track_id as id, t.slug_id, t.track_title as _name, null as num_tracks, t.track_plays as plays,
         t.track_bpm as bpm, t.image_url, t.audio_url, tl.license_attr, t.field_settings,
-        ta.artist_name as artist_name, t.created_at,
+        ta.artist_name as artist_name, ta.artist_slug as artist_slug, g.genre_slug as genre_slug,
+        t.created_at,
         1 as is_track, CONCAT_WS('/', '/tracks', t.slug_id, t.track_slug) as _link")
-                ->From("$trackTable t")
-                ->Join("$trackTracksCategoriesTable ttc", "t.track_id", "ttc.fk_track_id")
-                ->Join("$trackCategoriesTable ct", "ttc.fk_track_cat_id", "ct.track_cat_id")
-                ->Join("$licenseTable tl", "tl.license_id", "t.fk_license_id")
-                ->Join("$artistTable ta", "ta.artist_id", "t.fk_artist_id")
-                ->WhereEquals('ct.track_cat_id', $mainTrackData->track_cat_id)
-                ->Raw('UNION')
-                ->Select("ct.track_cat_id as id, ct.slug_id, ct.track_cat_name as _name,
-        (SELECT COUNT(*) FROM $trackTracksCategoriesTable ttc
-        INNER JOIN $trackTable t ON ttc.fk_track_id = t.track_id
+                    ->From("{$this->getTrackTable()} t")
+                    ->Join("{$this->getTrackGenreTable()} tg", "tg.fk_track_id", "t.track_id")
+                    ->Join("{$this->getGenreTable()} g", "g.genre_id", "tg.fk_genre_id")
+                    ->Join("{$this->getTrackTracksCategoriesTable()} ttc", "t.track_id", "ttc.fk_track_id")
+                    ->Join("{$this->getTrackCategoriesTable()} ct", "ttc.fk_track_cat_id", "ct.track_cat_id")
+                    ->Join("{$this->getLicenseTable()} tl", "tl.license_id", "t.fk_license_id")
+                    ->Join("{$this->getArtistTable()} ta", "ta.artist_id", "t.fk_artist_id")
+                    ->WhereEquals('ct.track_cat_id', $mainTrackData->track_cat_id)
+                    ->Raw('UNION')
+                    ->Select("ct.track_cat_id as id, ct.slug_id, ct.track_cat_name as _name,
+        (SELECT COUNT(*) FROM {$this->getTrackTracksCategoriesTable()} ttc
+        INNER JOIN {$this->getTrackTable()} t ON ttc.fk_track_id = t.track_id
         WHERE ttc.fk_track_cat_id = ct.track_cat_id) as num_tracks, null as plays,
-        null as bpm, null as image_url, null as audio_url, null as license_attr, ct.field_settings, null as artist_name,
+        null as bpm, null as image_url, null as audio_url, null as license_attr, ct.field_settings, 
+        null as artist_name, null as artist_slug, null as genre_slug,
         null as created_at,
         0 as is_track, CONCAT_WS('/', '/track_categories', ct.slug_id, ct.track_cat_slug) as _link")
-                ->From("$trackCategoriesTable ct")
-                ->WhereEquals('ct.track_cat_parent_id', $mainTrackData->track_cat_id))
-                ->As('track_results')
-                ->OrderByAsc("is_track")
-                ->OrderByDesc("track_results.created_at")
-                ->SimplePaginate(AppConfig::getAppPaginationMax());
+                    ->From("{$this->getTrackCategoriesTable()} ct")
+                    ->WhereEquals('ct.track_cat_parent_id', $mainTrackData->track_cat_id))
+                    ->As('track_results')
+                    ->when(is_array(url()->getParam('track_bpm')), function (TonicsQuery $db){
 
-            $fieldSettingsForMainTrackData = json_decode($mainTrackData->field_settings, true);
-            $pageTemplate->getFieldData()->unwrapFieldContent($fieldSettingsForMainTrackData, contentKey: 'track_cat_content');
-            $mainTrackData = [...$fieldSettingsForMainTrackData, ...(array)$mainTrackData];
+                        $db->WhereIn('track_results.bpm', url()->getParam('track_bpm'));
+                    })->when(is_array(url()->getParam('track_genres')), function (TonicsQuery $db){
 
-            # Get Filters of a Certain Category and Its Sub Category
-            $this->handleFilterFromFieldSettingsKeyForCategorySubCategory($mainTrackData, $fieldSettings);
-            $this->handleFilterTrackArtistKeyForCategorySubCategory($mainTrackData, $fieldSettings);
-            $this->handleFilterTrackGenreKeyForCategorySubCategory($mainTrackData, $fieldSettings);
+                        $db->WhereIn('track_results.genre_slug', url()->getParam('track_genres'));
+                    })->when(url()->hasParamAndValue('track_artist'), function (TonicsQuery $db){
 
+                        $db->WhereEquals('track_results.artist_slug', url()->getParam('track_artist'));
+                    })->when(url()->hasParamAndValue('track_key'), function (TonicsQuery $db) {
 
-           // dd($filterOptions, $mainTrackData);
-            $fieldSettings['ThemeFolder'] = $data;
-            $fieldSettings['MainTrackData'] = $mainTrackData;
-            // dd($fieldSettings);
-            $pageTemplate->setFieldSettings($fieldSettings);
+                        $trackKey = '"' . url()->getParam('track_key') . '"';
+                        $db->WhereJsonContains('track_results.field_settings', 'track_default_filter_keys', $trackKey);
+                    })->when(url()->hasParamAndValue('query'), function (TonicsQuery $db) {
+                        $db->WhereLike('_name', url()->getParam('query'));
+                    });
+
+                $data =  $this->dbWhenForCommonFieldKey($db)
+                    ->GroupBy("slug_id")
+                    ->OrderByAsc("is_track")
+                    ->OrderByDesc("created_at")
+                    ->SimplePaginate(AppConfig::getAppPaginationMax());
+
+                $fieldSettingsForMainTrackData = json_decode($mainTrackData->field_settings, true);
+                $pageTemplate->getFieldData()->unwrapFieldContent($fieldSettingsForMainTrackData, contentKey: 'track_cat_content');
+                $mainTrackData = [...$fieldSettingsForMainTrackData, ...(array)$mainTrackData];
+
+                # Get Filters of a Certain Category and Its Sub Category
+                $this->handleFilterFromFieldSettingsKeyForCategorySubCategory($mainTrackData, $fieldSettings);
+                $this->handleFilterTrackArtistKeyForCategorySubCategory($mainTrackData, $fieldSettings);
+                $this->handleFilterTrackGenreKeyForCategorySubCategory($mainTrackData, $fieldSettings);
+
+                $fieldSettings['ThemeFolder'] = $data;
+                $fieldSettings['MainTrackData'] = $mainTrackData;
+                $pageTemplate->setFieldSettings($fieldSettings);
+            } catch (\Exception $exception){
+                // Log...
+            }
 
         }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function dbWhenForCommonFieldKey(TonicsQuery $db): TonicsQuery
+    {
+        $keys = [
+            'track_default_filter_mood',
+            'track_default_filter_instruments',
+            'track_default_filter_samplePacks_Type',
+            'track_default_filter_acapella_gender',
+            'track_default_filter_acapella_vocalStyle',
+            'track_default_filter_acapella_emotion',
+            'track_default_filter_acapella_scale',
+            'track_default_filter_acapella_effects'
+        ];
+        foreach ($keys as $key){
+            $db->when(is_array(url()->getParam($key)), function (TonicsQuery $db) use ($key) {
+                $keyValues = url()->getParam($key);
+                foreach ($keyValues as $value){
+                    $value = '"' . $value . '"';
+                    $db->WhereJsonContains('track_results.field_settings', $key, $value, ifWhereUse: 'OR');
+                }
+            });
+        }
+
+        return $db;
     }
 
 
