724044f7be7ddba929fcb5520041d6a2aebdae93
DevsrealmGuy <faruq@devsrealm.com>
2023-03-09 13:46:19 +0100

    Update Changes

---
+++ src/Apps/NinetySeven/Database/Migrations/TonicsNinetysevenDeleteFieldSlugPostHomePage_2023_03_09_134218.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Copyright (c) 2022. Ahmed Olayemi Faruq <faruq@devsrealm.com>
+ */
+
+namespace App\Apps\NinetySeven\Database\Migrations;
+
+use App\Modules\Core\Library\Migration;
+use App\Modules\Core\Library\Tables;
+use Devsrealm\TonicsQueryBuilder\TonicsQuery;
+
+class TonicsNinetysevenDeleteFieldSlugPostHomePage_2023_03_09_134218 extends Migration {
+
+    /**
+     * @return void
+     * @throws \Exception
+     */
+    public function up()
+    {
+        $toDelete = ['app-ninety-seven-post-home-page'];
+        db(onGetDB: function (TonicsQuery $db) use ($toDelete) {
+            $newDB = db();
+            $db->FastDelete($this->getFieldTable(), $newDB->WhereIn(table()->getColumn($this->getFieldTable(), 'field_slug'), $toDelete));
+            $newDB->getTonicsQueryBuilder()->destroyPdoConnection();
+        });
+    }
+
+    public function down()
+    {
+        $this->getDB()->run("");
+    }
+
+    public function getFieldTable(): string
+    {
+        return Tables::getTable(Tables::FIELD);
+    }
+}
