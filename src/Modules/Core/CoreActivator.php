26bea45ca295c6ed4c48460d2a8016bc37a12da7
Olayemi Faruq <olayemi@tonics.app>
2024-06-28 15:13:38 +0100

    [Version] 🔢 Increased Version Number

--- src/Modules/Core/CoreActivator.php
+++ src/Modules/Core/CoreActivator.php
@@ -195,7 +195,7 @@ class CoreActivator implements ExtensionConfig, FieldItemsExtensionConfig
             "type"                 => "Module",
             "slug_id"              => "166f7ea3-2703-11ef-9736-124c30cfdb6b",
             // the first portion is the version number, the second is the code name and the last is the timestamp
-            "version"              => '1-O-Ola.1718718675',
+            "version"              => '1-O-Ola.1718718676',
             "stable"               => 0,
             "description"          => "The Core Module",
             "info_url"             => '',
