debc9350a9c1a070008084c15e48340d4fc6ccdc
Olayemi Faruq <olayemi@tonics.app>
2024-07-01 23:17:29 +0100

    [Feature] New Role Permission for getFieldItems and associated permission for core and customer role

---
+++ src/Modules/Field/RequestInterceptor/FieldRenderAccess.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ *     Copyright (c) 2022-2024. Olayemi Faruq <olayemi@tonics.app>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace App\Modules\Field\RequestInterceptor;
+
+use App\Modules\Core\Data\UserData;
+use App\Modules\Core\Library\Authentication\Roles;
+use App\Modules\Core\Library\SimpleState;
+use Devsrealm\TonicsRouterSystem\Events\OnRequestProcess;
+use Devsrealm\TonicsRouterSystem\Interfaces\TonicsRouterRequestInterceptorInterface;
+
+class FieldRenderAccess implements TonicsRouterRequestInterceptorInterface
+{
+
+    /**
+     * @inheritDoc
+     * @throws \Exception
+     * @throws \Throwable
+     */
+    public function handle (OnRequestProcess $request): void
+    {
+        if (UserData::canAccess(Roles::CAN_RENDER_FIELDS) === false) {
+            SimpleState::displayUnauthorizedErrorMessage();
+        }
+    }
+}
